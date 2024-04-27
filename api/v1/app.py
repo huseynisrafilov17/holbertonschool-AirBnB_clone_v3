2fcd03978fb2b0a8e6757ab2989f667fe02defd5
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-04-27 22:45:00 +0400

    6th task - Status code

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ app.register_blueprint(app_views)
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return jsonify({"error": "Not found"})
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.teardown_appcontext
